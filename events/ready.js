d660c00a6a698b17c29fc377af887f185114d70b
hab927 <47962153+hab927@users.noreply.github.com>
2025-09-25 10:41:26 -0700

    replies used the old system whoops

--- events/ready.js
+++ events/ready.js
@@ -42,74 +42,82 @@ module.exports = {
 				}
 
 				msg = await message.content;
-	
-				if (msg.match(imMatch1)) {
-					nick = await msg.match(imMatch1)[2].slice(0,32);
-				}
-				else if (msg.match(imMatch2)) {
-					nick = await msg.match(imMatch2)[2].slice(0,32);
-				}
-				else if (msg.match(imMatch3)) {
-					nick = await msg.match(imMatch3)[2].slice(0,32);
-				}
-	
-				if (nick) {
-					await msgAuthor.setNickname(nick)
-						.then(console.log(`${msgAuthor.user.username} changed their nickname to ${nick}: "${message.content}"`))
-						.catch(console.error);
-				}
 
-				// want to make the first person who spoke change the name of the second
-				else if (msg.match(youreMatch1)) {
-					nick = await msg.match(youreMatch1)[2].slice(0,32);
-					youre = true;
-				}
-				else if (msg.match(youreMatch2)) {
-					nick = await msg.match(youreMatch2)[2].slice(0,32);
-					youre = true;
-				}
-				else if (msg.match(youreMatch3)) {
-					nick = await msg.match(youreMatch3)[2].slice(0,32);
-					youre = true;
-				}
-				else if (msg.match(youreMatch4)) {
-					nick = await msg.match(youreMatch4)[2].slice(0,32);
-					youre = true;
-				}
-				else if (msg.match(youreMatch5)) {
-					nick = await msg.match(youreMatch5)[2].slice(0,32);
-					youre = true;
-				}
-
-				if (speakingOrder[0] && speakingOrder[1] && nick && youre && (channelOrder[0] === channelOrder[1])) {
-					await speakingOrder[1].setNickname(nick)
-						.then(console.log(`${speakingOrder[0].user.username} changed ${speakingOrder[1].user.username}'s nickname to ${nick}: "${message.content}"`))
-						.catch(console.error);
-					youre = false;
-				}
-	
 				// reply check
 				if (message.reference) {
-					let replynick = "";
-					if (message.content.toLowerCase().startsWith("youre ")) {
-						replynick = await message.content.substring(6,38);
+					let nick = "";
+					if (msg.match(youreMatch1)) {
+						nick = await msg.match(youreMatch1)[2].slice(0,32);
 					}
-					else if (message.content.toLowerCase().startsWith("you're ")) {
-						replynick = await message.content.substring(7,39);
+					else if (msg.match(youreMatch2)) {
+						nick = await msg.match(youreMatch2)[2].slice(0,32);
 					}
-					else if (message.content.toLowerCase().startsWith("ur ")) {
-						replynick = await message.content.substring(3,35);
+					else if (msg.match(youreMatch3)) {
+						nick = await msg.match(youreMatch3)[2].slice(0,32);
 					}
-					if (replynick) {
+					else if (msg.match(youreMatch4)) {
+						nick = await msg.match(youreMatch4)[2].slice(0,32);
+					}
+					else if (msg.match(youreMatch5)) {
+						nick = await msg.match(youreMatch5)[2].slice(0,32);
+					}
+					if (nick) {
 						const repliedmsg = await message.fetchReference();
 						const target = await msgGuild.members.cache.get(repliedmsg.author.id);
 						if (target) {
-							await target.setNickname(replynick)
-								.then(console.log(`${msgAuthor.user.username} changed ${target.user.username}'s nickname to ${replynick}`))
+							await target.setNickname(nick)
+								.then(console.log(`${msgAuthor.user.username} changed ${target.user.username}'s nickname to ${nick}`))
 								.catch(console.error);
 						}
 					}
 				}
+				else {
+					if (msg.match(imMatch1)) {
+						nick = await msg.match(imMatch1)[2].slice(0,32);
+					}
+					else if (msg.match(imMatch2)) {
+						nick = await msg.match(imMatch2)[2].slice(0,32);
+					}
+					else if (msg.match(imMatch3)) {
+						nick = await msg.match(imMatch3)[2].slice(0,32);
+					}
+		
+					if (nick) {
+						await msgAuthor.setNickname(nick)
+							.then(console.log(`${msgAuthor.user.username} changed their nickname to ${nick}: "${message.content}"`))
+							.catch(console.error);
+					}
+	
+					// want to make the first person who spoke change the name of the second
+					else if (msg.match(youreMatch1)) {
+						nick = await msg.match(youreMatch1)[2].slice(0,32);
+						youre = true;
+					}
+					else if (msg.match(youreMatch2)) {
+						nick = await msg.match(youreMatch2)[2].slice(0,32);
+						youre = true;
+					}
+					else if (msg.match(youreMatch3)) {
+						nick = await msg.match(youreMatch3)[2].slice(0,32);
+						youre = true;
+					}
+					else if (msg.match(youreMatch4)) {
+						nick = await msg.match(youreMatch4)[2].slice(0,32);
+						youre = true;
+					}
+					else if (msg.match(youreMatch5)) {
+						nick = await msg.match(youreMatch5)[2].slice(0,32);
+						youre = true;
+					}
+	
+					if (speakingOrder[0] && speakingOrder[1] && nick && youre && (channelOrder[0] === channelOrder[1])) {
+						await speakingOrder[1].setNickname(nick)
+							.then(console.log(`${speakingOrder[0].user.username} changed ${speakingOrder[1].user.username}'s nickname to ${nick}: "${message.content}"`))
+							.catch(console.error);
+						youre = false;
+					}
+				}
+	
 			}
 			catch(error) {
 				console.log(error);
